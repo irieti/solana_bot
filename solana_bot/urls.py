11b4ea571267a66a770f3e1faf5aee4ac0a692f7
irieti <irieid@icloud.com>
2025-02-09 19:56:07 +0300

    webhook url -

--- solana_bot/urls.py
+++ solana_bot/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path("get-loop-updates/", views.get_loop_updates, name="get-loop-updates"),
     path("new-wallet/", views.new_wallet, name="new-wallet"),
     path("get_wallets/", views.get_wallets, name="get_wallets"),
-    path("webhook/", views.webhook, name="webhook"),
 ]
